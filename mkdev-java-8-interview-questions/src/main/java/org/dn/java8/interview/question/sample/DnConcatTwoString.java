5c1efa76b87e6eb4a93714ae178c52238c81e5ac
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2023-07-07 00:48:55 +0530

    Initial code commit.

---
+++ mkdev-java-8-interview-questions/src/main/java/org/dn/java8/interview/question/sample/DnConcatTwoString.java
@@ -0,0 +1,22 @@
+package org.dn.java8.interview.question.sample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/***
+ * @author Muthukumar Thangavinayagam.
+ * Created on 07-07-2023
+ ***/
+
+public class DnConcatTwoString {
+
+    public static void main(String[] args) {
+        List<String> list1 = Arrays.asList("Java", "8");
+        List<String> list2 = Arrays.asList("explained", "through", "programs");
+
+        Stream<String> concatStream = Stream.concat(list1.stream(), list2.stream());
+        concatStream.forEach(str -> System.out.print(str + " "));
+    }
+
+}
