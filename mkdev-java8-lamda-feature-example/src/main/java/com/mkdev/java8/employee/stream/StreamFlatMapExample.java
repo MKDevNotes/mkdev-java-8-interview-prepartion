01d2d0921d2b1d836645af4bba98dd8c9be97a5c
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2023-07-11 22:11:34 +0530

    Stream FlatMap committed.

--- mkdev-java8-lamda-feature-example/src/main/java/com/mkdev/java8/employee/stream/StreamFlatMapExample.java
+++ mkdev-java8-lamda-feature-example/src/main/java/com/mkdev/java8/employee/stream/StreamFlatMapExample.java
@@ -4,19 +4,61 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.mkdev.java8.employee.repository.PersonRepository;
+import com.mkdev.java8.employee.repository.model.Person;
+
+/**
+ * @author Muthukumar Thangavinayagam
+ *
+ */
 public class StreamFlatMapExample {
+	
+	static List<String> getPersonWithDuplicateHobbies(List<Person> personlist) {
+		 return personlist.stream()
+						.map(Person :: getHobbies)
+						.flatMap(List :: stream)
+						.collect(Collectors.toList());
+	}
+	
+	
+	static List<String> getPersonHobbies(List<Person> personlist) {
+		 return personlist.stream()
+						.map(Person :: getHobbies)
+						.flatMap(List :: stream)
+						.distinct()
+						.collect(Collectors.toList());
+	}
+	
+	static List<String> getPersonSortedHobbies(List<Person> personlist) {
+		 return personlist.stream()
+						.map(Person :: getHobbies)
+						.flatMap(List :: stream)
+						.distinct()
+						.sorted()
+						.collect(Collectors.toList());
+	}
+	
+	static long getPersonCountHobbies(List<Person> personlist) {
+		 return personlist.stream()
+						.map(Person :: getHobbies)
+						.flatMap(List :: stream)
+						.distinct()
+						.sorted()
+						.count();
+	}
+	
 	public static void main(String[] args) {
-		List<Integer> oddNumber = Arrays.asList(1,3,5,7);
-		List<Integer> evenNumber = Arrays.asList(2,4,6,8);
-		
-		List<List<Integer>> listoflist = Arrays.asList(oddNumber,evenNumber);
-		System.out.println("List combine "+listoflist);
-		
-		List<Integer> flattenList = listoflist.stream()
-											   .flatMap(list -> list.stream())
-											   .sorted()
-											   .collect(Collectors.toList());
-		
-		flattenList.forEach( i -> System.out.print(" "+i));
+	
+		System.out.println(" ======= Get All Hobbies ======= ");
+		getPersonWithDuplicateHobbies(PersonRepository.getAllPersons()).forEach( h -> System.out.print(" "+h));
+		System.out.println(" ");
+		System.out.println(" ======= Get Distinct Hobbies ======= ");
+		getPersonHobbies(PersonRepository.getAllPersons()).forEach( h -> System.out.print(" "+h));
+		System.out.println(" ");
+		System.out.println(" ======= Get Distinct sorted Hobbies ======= ");
+		getPersonSortedHobbies(PersonRepository.getAllPersons()).forEach( h -> System.out.print(" "+h));
+		System.out.println(" ");
+		System.out.println(" ======= Get Distinct Hobbies Count ======= ");
+		System.out.println(" Count :- "+getPersonCountHobbies(PersonRepository.getAllPersons()));
 	}
 }
