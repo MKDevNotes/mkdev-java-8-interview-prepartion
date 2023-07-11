ce3bd6d9101bff60bae8b1029be0b1a04d64ab48
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2023-07-11 21:36:11 +0530

    Stream Flatmap example code commit.

---
+++ mkdev-java8-lamda-feature-example/src/main/java/com/mkdev/java8/employee/stream/StreamFlatMapExample.java
@@ -0,0 +1,22 @@
+package com.mkdev.java8.employee.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFlatMapExample {
+	public static void main(String[] args) {
+		List<Integer> oddNumber = Arrays.asList(1,3,5,7);
+		List<Integer> evenNumber = Arrays.asList(2,4,6,8);
+		
+		List<List<Integer>> listoflist = Arrays.asList(oddNumber,evenNumber);
+		System.out.println("List combine "+listoflist);
+		
+		List<Integer> flattenList = listoflist.stream()
+											   .flatMap(list -> list.stream())
+											   .sorted()
+											   .collect(Collectors.toList());
+		
+		flattenList.forEach( i -> System.out.print(" "+i));
+	}
+}
