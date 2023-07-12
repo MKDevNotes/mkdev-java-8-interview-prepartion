cf7acf74dcc23d99bb70b937427f6fe3dde91b35
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2023-07-12 21:13:54 +0530

    Code committed for FindAny and FindFist

---
+++ mkdev-java8-lamda-feature-example/src/main/java/com/mkdev/java8/employee/stream/StreamFindAnyAndFindFistExample.java
@@ -0,0 +1,19 @@
+package com.mkdev.java8.employee.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Muthukumar Thangavinayagam
+ *
+ */
+public class StreamFindAnyAndFindFistExample {
+
+	public static void main(String[] args) {
+		List<String> fruits = Arrays.asList("Apple","Orange","Banana","Pineapple","Grape","Watermelon");
+		
+		System.out.println("Find Any "+fruits.stream().filter(s -> s.length() > 5).findAny());
+		System.out.println("Find Fist "+fruits.stream().filter(s -> s.length() == 5).findFirst());
+	}
+	
+}
